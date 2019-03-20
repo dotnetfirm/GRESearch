42383355d712fe06e3fc85ecb567f6654768ce38
rafi-khan <n.zamank.rk@gmail.com>
2019-03-20 15:44:26 +0600

    Helper

---
+++ Helper/OxfordClientHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Net;
+namespace GRESearch.Helper
+{
+    public class OxfordClientHelper
+    {
+        public string SearchWord(string url, string app_id, string app_key)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var content = string.Empty;
+            request.Method = "GET";
+            request.ContentType = "application/json";
+
+            if (!string.IsNullOrEmpty(app_id))
+            {
+                request.Headers.Add("app_id:" + app_id);
+            }
+            if (!string.IsNullOrEmpty(app_key))
+            {
+                request.Headers.Add("app_key:" + app_key);
+            }
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                }
+            }
+
+            return content;
+        }
+    }
+}
